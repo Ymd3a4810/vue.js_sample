d0fee42b5ca92f709ed2437a65acb36b529c059c
Yamadalinux4810 <080100704.33runner@gmail.com>
2023-07-14 18:45:06 +0900

    issueの追加

--- todo-project/src/App.vue
+++ todo-project/src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <div id="app">
-    <nav>
+    <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+      <router-link to="/todo-list">TodoList</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
+
 
 <style>
 #app {
---
+++ todo-project/src/views/IssueList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>issueリスト</h1>
+    <el-button type="success">issue取得</el-button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'; // --1
+
+export default {
+  name: 'IssueList',
+}
+</script>
